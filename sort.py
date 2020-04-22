daf917a9c99cb012ad2666b1ba82a0a124d7208b
Aditya <aditya.baravkar@gmail.com>
2020-04-22 21:52:15 +0530

    Dictionary of FILE_FORMATS

--- sort.py
+++ sort.py
@@ -24,4 +24,6 @@ DIRECTORIES = {
   
 } 
   
-
+FILE_FORMATS = {file_format: directory 
+                for directory, file_formats in DIRECTORIES.items() 
+                for file_format in file_formats}
